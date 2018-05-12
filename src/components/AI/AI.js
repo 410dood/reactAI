9731b959b62ed7e87f8af32d00cc8602f6ac0f50
Bill Doody <wdoody@yahoo.com>
2018-05-12 16:40:10 -0600

    Last working before refactor

--- src/components/AI/AI.js
+++ src/components/AI/AI.js
@@ -4,27 +4,25 @@
 // data     }     clarifai         .models .predict(Clarifai.GENERAL_MODEL,
 // file)         .then(response => {  const {concepts} =
 // response.outputs[0].data             if (concepts && concepts.length > 0) {
-//               for (const prediction of concepts) {                   if
-// (prediction.name === 'pizza' && prediction.value >= 0.99) {
-//       return this.setState({loading: false, result: 'Pizza'})
-//     }                     this.setState({result: 'Not Pizza'})
-//  }             } this.setState({loading: false})         })         .catch(e
-// => { Alert.alert('Une erreur est survenue', 'Désolé, le quota est peut-être
-// dépassé, réessaye plus tard !', [                 {                     text:
-// 'OK',                     onPress: () => this._cancel()                 }
-//     ], {cancelable: false})         }) } _cancel() {     const backAction =
-// NavigationActions.back()     this         .props         .navigation
-// .dispatch(backAction) } render() {     const {type, data} =
-// this.props.navigation.state.params.image     const sourceImage =
-// `data:${type};base64,${data}`     return (         <BackgroundImage
+//             for (const prediction of concepts) {                   if
+// (prediction.name === 'pizza' && prediction.value >= 0.99) {       return
+// this.setState({loading: false, result: 'Pizza'})     }
+// this.setState({result: 'Not Pizza'})  }             } this.setState({loading:
+// false})         })         .catch(e => { Alert.alert('Une erreur est
+// survenue', 'Désolé, le quota est peut-être dépassé, réessaye plus tard !', [
+//                {                     text: 'OK',                     onPress:
+// () => this._cancel()                 }     ], {cancelable: false})         })
+// } _cancel() {     const backAction = NavigationActions.back()     this
+//  .props         .navigation .dispatch(backAction) } render() {     const
+// {type, data} = this.props.navigation.state.params.image     const sourceImage
+// = `data:${type};base64,${data}`     return (         <BackgroundImage
 // source={{             uri: sourceImage         }} resizeMode='cover'>
-//     <StatusBar hidden/> {this.state.loading           ? <View
+// <StatusBar hidden/> {this.state.loading           ? <View
 // style={styles.loader}> <ActivityIndicator size={75} color='#95a5a6'/>
-//                 <Text style={styles.loaderText}>Analyse en cours...</Text>
-// </View>                 : <View style={styles.container}> <AnswerNotification
-// answer={this.state.result}/> <CaptureAndShare
-// title='Partager'     color='#3498db'
-// image={sourceImage}           onCancel={this._cancel}/>
+//         <Text style={styles.loaderText}>Analyse en cours...</Text> </View>
+//              : <View style={styles.container}> <AnswerNotification
+// answer={this.state.result}/> <CaptureAndShare title='Partager'
+// color='#3498db' image={sourceImage}           onCancel={this._cancel}/>
 // <XPButton title='Non merci' color='black' textOnly onPress={this._cancel}/>
 // </View> }         </BackgroundImage>     ) } } PredictScreen.propTypes = {
 // navigation: PropTypes.object } export default PredictScreen
