dd8de85fb658addd5bc35ce7e18d0dbb4fcb9bb1
Bill Doody <wdoody@yahoo.com>
2018-05-12 22:36:49 -0600

    delete webpackconfig

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,34 +1,19 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {Navbar} from 'react-bootstrap';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import Home from './Home/Home';
-import Main from './FileUpload/FileUpload.js';
-import ImageLinkForm from './ImageLinkForm/ImageLinkForm';
 class Navigation extends Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Navbar>
-                        <Navbar.Header>
-                            <Navbar.Brand>
-                                <Link to='/'>HeavyDoodyAI</Link>
-
-                                <Link to={'/Main'}>FileUpload</Link>
-                                <Link to={'/ImageLinkForm'}>ImageLinkForm</Link>
-                            </Navbar.Brand>
-                        </Navbar.Header>
-                    </Navbar>;
-                    <Switch >
-                        <Route exact path='/' component={Home}/>
-                        <Route path='/FileUpload' component={Main}/> {/* <Route path='/AI' component={AI}/> */}
-                        <Route path='/ImageLinkForm' component={ImageLinkForm}/>
-                    </Switch>
-                </div>
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to='/'>Clarifai Demo</Link>
+          </Navbar.Brand>
+        </Navbar.Header>
+      </Navbar>
+    );
+  }
 }
-export default Navigation
+
+export default Navigation;
