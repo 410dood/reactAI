7b5f8e74953aa4e5a839e378f10ac0a5acbba1ff
Bill Doody <wdoody@yahoo.com>
2018-05-20 13:52:45 -0600

    trying airbnb style

--- src/server/index.js
+++ src/server/index.js
@@ -5,8 +5,15 @@ const session = require('express-session')
 const dbConnection = require('./database')
 const MongoStore = require('connect-mongo')(session)
 const passport = require('./passport');
-const app = express()
-const PORT = 8080
+const app = express();
+const os = require('os');
+
+
+// const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
+
+// this is to serve the frontend part
+app.use(express.static('build/'));
 // Route requires
 const user = require('./routes/user')
 
@@ -22,7 +29,7 @@ app.use(bodyParser.json())
 // Sessions
 app.use(
 	session({
-		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+		secret: 'liam', //pick a random string to make the hash that is generated secure
 		store: new MongoStore({ mongooseConnection: dbConnection }),
 		resave: false, //required
 		saveUninitialized: false //required
@@ -41,3 +48,16 @@ app.use('/user', user)
 app.listen(PORT, () => {
 	console.log(`App listening on PORT: ${PORT}`)
 })
+
+
+
+
+
+// const express = require('express');
+// const os = require('os');
+
+// const app = express();
+
+// app.use(express.static('dist'));
+// app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+// app.listen(8080, () => console.log('Listening on port 8080!'));
